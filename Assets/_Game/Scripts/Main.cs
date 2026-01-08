2ba7e86d6e728a99bbafc8829fdbf06f9065b825
Sekiro <2293073106@qq.com>
2026-01-08 14:22:36 +0800

    接入ab

---
+++ Assets/_Game/Scripts/Main.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour
+{
+    private void Awake()
+    {
+        ResourceManage.Instance.Init();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject cube = ResourceManage.Instance.LoadRes<GameObject>("test_obj", "cube");
+        Instantiate(cube);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/_Game/Scripts/Manager/ResourceManager.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResourceManage
+{
+    private static ResourceManage _instance;
+    public static ResourceManage Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = new ResourceManage();
+            }
+            return _instance;
+        }
+    }
+
+    private Dictionary<string, AssetBundle> resourceCache = new Dictionary<string, AssetBundle>();
+    private AssetBundleManifest manifest;
+    public void Init()
+    {
+        AssetBundle ab = AssetBundle.LoadFromFile(Application.streamingAssetsPath + "/" + "Android");
+        manifest = ab.LoadAsset<AssetBundleManifest>("AssetBundleManifest");
+    }
+
+    private AssetBundle LoadABRes(string resourceName)
+    {
+        if(resourceCache.ContainsKey(resourceName))
+        {
+            return resourceCache[resourceName];
+        }
+        // Unity 底层对 Android 的 LoadFromFile 做了特殊处理，能直接读 APK 里的资源
+        AssetBundle ab = AssetBundle.LoadFromFile(Application.streamingAssetsPath + "/" + resourceName);
+        resourceCache.Add(resourceName, ab);
+
+        string[] dependencies = manifest.GetAllDependencies(resourceName);
+        foreach(string depend in dependencies)
+        {
+            if (!resourceCache.ContainsKey(depend))
+            {
+                LoadABRes(depend);
+            }
+        }
+
+        return ab;
+    }
+
+    /// <summary>
+    /// 加载ab中的资源
+    /// </summary>
+    /// <typeparam name="T">资源类型</typeparam>
+    /// <param name="packageName">ab包名</param>
+    /// <param name="resourceName">资源名</param>
+    /// <returns></returns>
+    public T LoadRes<T>(string packageName, string resourceName) where T : Object
+    {
+        AssetBundle ab = LoadABRes(packageName);
+        if (ab == null)
+        {
+            Debug.LogError("Load AssetBundle Failed: " + packageName);
+            return null;
+        }
+        return ab.LoadAsset<T>(resourceName);
+    }
+}
