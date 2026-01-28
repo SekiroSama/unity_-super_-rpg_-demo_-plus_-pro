e26b14f798d729d99f14817d249fb7e4016e4edd
Sekiro <2293073106@qq.com>
2026-01-28 14:45:28 +0800

    1

---
+++ Assets/_Game/Scripts/开发日志.cs
@@ -0,0 +1,18 @@
+﻿
+using System.Collections.Generic;
+
+public class 开发日志
+{
+    List<string> toDoList = new List<string>()
+    {
+        "武器的刀光效果：需要在动画事件中控制TrailRenderer的开启和关闭"
+    };
+
+
+    string Last = "完成了武器的刀光基本效果。";
+    string Doing = "用插值顶点让刀光变圆";
+
+
+}
+
+
