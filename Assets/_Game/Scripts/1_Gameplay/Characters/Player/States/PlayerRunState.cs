1252cfe36a27873622988be5d28203016bca535f
Bababoi-bobo <gilltenney068@gmail.com>
2026-02-03 09:43:53 +0800

    1

--- Assets/_Game/Scripts/1_Gameplay/Characters/Player/States/PlayerRunState.cs
+++ Assets/_Game/Scripts/1_Gameplay/Characters/Player/States/PlayerRunState.cs
@@ -7,27 +7,27 @@ public class PlayerRunState: PlayerGroundState
     public override void OnEnter()
     {
         owner.PlayAnimation(AnimationConfig.StateHashes.Locomotion, AnimationConfig.TransitionSettings.NormalTransitionDuration);
-        GameManager.Instance.runTimer = 0;
+        //GameManager.Instance.runTimer = 0;
     }
     public override void OnUpdate()
     {
         base.OnUpdate();
-        if (GameManager.Instance.runTimer<owner.fadeTime)
-        {
-           owner.moveSpeed = Mathf.Lerp(owner.walkSpeed, owner.runSpeed, GameManager.Instance.runTimer/owner.fadeTime);
-        }
+        //if (GameManager.Instance.runTimer<owner.fadeTime)
+        //{
+        //   owner.moveSpeed = Mathf.Lerp(owner.walkSpeed, owner.runSpeed, GameManager.Instance.runTimer/owner.fadeTime);
+        //}
         if (!GameManager.Instance.inputManager.CurrentInput.IsRun)
         {
             stateMachine.ChangeState<PlayerMoveState>();
             return;
         }
 
-        owner.UpdateLocomotion(Mathf.Lerp(inputValue,1.5f,GameManager.Instance.runTimer/owner.fadeTime));
+        //owner.UpdateLocomotion(Mathf.Lerp(inputValue,1.5f,GameManager.Instance.runTimer/owner.fadeTime));
         owner.Move(GameManager.Instance.inputManager.CurrentInput.MoveVector);
     }
     public override void OnExit() 
     {
-        GameManager.Instance.runTimer = 0;
+        //GameManager.Instance.runTimer = 0;
         runValue = 1.5f;
         owner.moveSpeed = owner.walkSpeed;
     }
