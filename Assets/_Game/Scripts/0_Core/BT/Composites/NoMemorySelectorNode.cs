a2b334519c94d53cea59d97da29363b49e001fe1
Sekiro <2293073106@qq.com>
2026-02-04 17:36:02 +0800

    Update NoMemorySelectorNode.cs

--- Assets/_Game/Scripts/0_Core/BT/Composites/NoMemorySelectorNode.cs
+++ Assets/_Game/Scripts/0_Core/BT/Composites/NoMemorySelectorNode.cs
@@ -1,18 +1,41 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NoMemorySelectorNode : MonoBehaviour
+/// <summary>
+/// 无记忆选择节点 每次都会评估所有节点 失败继续成功返回 直到全部失败 选择一个能跑的跑
+/// </summary>
+public class NoMemorySelectorNode : BTNode
 {
-    // Start is called before the first frame update
-    void Start()
+    List<BTNode> childNodes;
+
+    /// <summary>
+    /// init
+    /// </summary>
+    /// <param name="childNodes">子节点</param>
+    public NoMemorySelectorNode(List<BTNode> childNodes)
     {
-        
+        this.childNodes = childNodes;
     }
 
-    // Update is called once per frame
-    void Update()
+    public override NodeStatus Evaluate(Blackboard blackboard)
     {
-        
+        for (int i = 0; i < childNodes.Count; i++)
+        {
+            switch (childNodes[i].Evaluate(blackboard))
+            {
+                case NodeStatus.SUCCESS:
+                    currentStatus = NodeStatus.SUCCESS;
+                    return currentStatus;
+                case NodeStatus.RUNNING:
+                    currentStatus = NodeStatus.RUNNING;
+                    return currentStatus;
+                case NodeStatus.FAILURE:
+                    currentStatus = NodeStatus.FAILURE;
+                    continue;
+            }
+        }
+        currentStatus = NodeStatus.FAILURE;
+        return currentStatus;
     }
 }
