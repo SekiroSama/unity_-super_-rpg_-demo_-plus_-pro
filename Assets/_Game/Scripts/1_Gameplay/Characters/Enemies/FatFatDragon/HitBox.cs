50f6981497f0e65588ea1175efff53246c2dc0ae
Sekiro <2293073106@qq.com>
2026-03-09 15:42:34 +0800

    1

---
+++ Assets/_Game/Scripts/1_Gameplay/Characters/Enemies/FatFatDragon/HitBox.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitBox : MonoBehaviour
+{
+    /// <summary>
+    /// Åö×²Âß¼­
+    /// </summary>
+    /// <param name="other"></param>
+    public void OnTriggerEnter(Collider other)
+    {
+        PlayerController playerController = other.GetComponent<PlayerController>();
+        
+    }
+}
